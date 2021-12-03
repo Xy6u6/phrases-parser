eaa82e02c6ec407f6dec58218cf5d3af1a1be5f2
xy6u6 <Anton.voroniuk@gmail.com>
2021-12-03 14:53:27 +0200

    refactor

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import logging as log
+import os
+
 import scrapper as sc
+from src.gcp import upload_to_cloud
 
 
 def main():
@@ -8,12 +11,20 @@ def main():
     urls = sc.get_top_ten_tags()
     log.info('got list of URLS')
     dict_of_quotes = {}
+    # scrapping urls
     for item in urls:
-        log.info(f'scrapping URLS: {urls.index(item)+1}th of {len(urls)} pages, {item}')
+        log.info(f'scrapping URLS: {urls.index(item) + 1}th of {len(urls)} pages, {item}')
         tag = item[32:len(item) - 6]
         dict_of_quotes[tag] = sc.parse_phrases(item)
+    # write to files
     sc.write_phrases_to_txt(dict_of_quotes)
+    # upload to gcp
+    list_of_files = os.listdir("/tmp/parser/")
+    for file in list_of_files:
+        log.info(f'uploading {file}')
+        upload_to_cloud("parser", "/tmp/parser/" + file, file)
     log.info('all done')
+
 
 if __name__ == '__main__':
     main()
