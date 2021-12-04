ca346d69eb2fb0a5b2ed7d60add4a00ed914407a
xy6u6 <Anton.voroniuk@gmail.com>
2021-12-04 22:59:27 +0200

    refactor

--- src/gcp.py
+++ src/gcp.py
@@ -15,7 +15,7 @@ def upload_to_cloud(bucket_name, source_file_name, destination_blob_name):
     # destination_blob_name = "storage-object-name"
     storage_client = storage.Client.from_service_account_json('tmp/gcp_acc.json')
     bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob("json/" + destination_blob_name)
+    blob = bucket.blob(destination_blob_name)
     blob.upload_from_filename(source_file_name)
     logging.info("File {} uploaded to {}.".format(source_file_name, destination_blob_name))
 
