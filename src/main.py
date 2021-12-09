b168190d67f82f4aa783353fab7d848a8107f9d4
xy6u6 <Anton.voroniuk@gmail.com>
2021-12-09 12:10:04 +0200

    refactor, split main func

--- src/main.py
+++ src/main.py
@@ -6,26 +6,35 @@ import scrapper as sc
 from gcp import upload_to_cloud, gcs_to_bq
 
 
-def main():
-    log.basicConfig(format='%(levelname)s:%(message)s', level='INFO')
-    log.info('run')
-    urls = sc.get_top_ten_tags()
-    log.info('got list of URLS')
+def parse_urls(urls):
     dict_of_quotes = {}
-    # scrapping urls
     for item in urls:
         log.info(f'scrapping URLS: {urls.index(item) + 1}th of {len(urls)} pages, {item}')
         tag = item[32:len(item) - 6]
         dict_of_quotes[tag] = sc.parse_phrases(item)
-    # write to files
-    sc.write_phrases_to_file(dict_of_quotes, 'csv')
-    # upload to gcp and bigquery
-    list_of_files = os.listdir()
+    return dict_of_quotes
+
+
+def upload_files_to_cloud():
+    list_of_files = os.listdir(FILES_PATH)
     for file in list_of_files:
         log.info(f'uploading to cloud storage {file}')
         local_file_path = FILES_PATH + file
         upload_to_cloud("parser", local_file_path, file)
         gcs_to_bq(file)
+
+
+def main():
+    log.basicConfig(format='%(levelname)s:%(message)s', level='INFO')
+    log.info('run')
+    urls = sc.get_top_ten_tags()
+    log.info('got list of URLS')
+    # scrapping urls into dict
+    dict_of_quotes = parse_urls(urls)
+    # write to files
+    sc.write_phrases_to_file(dict_of_quotes, 'csv')
+    # upload to gcp and bigquery
+    upload_files_to_cloud()
     log.info('all done')
 
 
